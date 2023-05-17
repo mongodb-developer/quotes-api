968f599b626cda22cd4076a98ac237b754b742ac
Massimiliano Marcon <massimiliano.marcon@mongodb.com>
2023-05-17 15:32:14 +0200

    Make ESlint happy

--- playgrounds/add-colors.mongodb.js
+++ playgrounds/add-colors.mongodb.js
@@ -1,4 +1,4 @@
-/* global use, db */
+/* global use, db, sleep */
 
 const fetch = require('node-fetch');
 
@@ -6,49 +6,48 @@ const fetch = require('node-fetch');
  *
  * This example uses the super cool Colormind API
  * http://colormind.io/api-access/
- * 
- *     █████████           ████                                      ███                 █████
- *   ███░░░░░███         ░░███                                     ░░░                 ░░███ 
- *  ███     ░░░   ██████  ░███   ██████  ████████  █████████████   ████  ████████    ███████ 
- * ░███          ███░░███ ░███  ███░░███░░███░░███░░███░░███░░███ ░░███ ░░███░░███  ███░░███ 
- * ░███         ░███ ░███ ░███ ░███ ░███ ░███ ░░░  ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███ 
- * ░░███     ███░███ ░███ ░███ ░███ ░███ ░███      ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███ 
- *  ░░█████████ ░░██████  █████░░██████  █████     █████░███ █████ █████ ████ █████░░████████
- *   ░░░░░░░░░   ░░░░░░  ░░░░░  ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░░░░ 
  *
- */ 
+ *     █████████           ████                                      ███                 █████
+ *   ███░░░░░███         ░░███                                     ░░░                 ░░███
+ *  ███     ░░░   ██████  ░███   ██████  ████████  █████████████   ████  ████████    ███████
+ * ░███          ███░░███ ░███  ███░░███░░███░░███░░███░░███░░███ ░░███ ░░███░░███  ███░░███
+ * ░███         ░███ ░███ ░███ ░███ ░███ ░███ ░░░  ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███
+ * ░░███     ███░███ ░███ ░███ ░███ ░███ ░███      ░███ ░███ ░███  ░███  ░███ ░███ ░███ ░███
+ *  ░░█████████ ░░██████  █████░░██████  █████     █████░███ █████ █████ ████ █████░░████████
+ *   ░░░░░░░░░   ░░░░░░  ░░░░░  ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░░░░
+ *
+ */
 
-const COLORS_API_URL = `http://colormind.io/api/`;
+const COLORS_API_URL = 'http://colormind.io/api/';
 
 use('quotes_db');
 const quotes = db.quotes.find().limit(50).toArray().map(q => ({ _id: q._id }));
 
-async function fetchInfo(quotes) {
+async function fetchInfo (quotes) {
   return Promise.all(quotes.map(async q => {
     try {
       sleep(Math.round(Math.random() * (2000 - 500) + 500));
       const response = await fetch(COLORS_API_URL, {
         method: 'post',
-        body: JSON.stringify({'model': 'paper_mario'})
+        body: JSON.stringify({ model: 'paper_mario' })
       });
-      if(response.ok) {
+      if (response.ok) {
         const data = await response.json();
-        return {...q, colors: data?.result }
+        return { ...q, colors: data?.result };
       }
       console.log(response.status);
-      return q;  
-    }
-    catch (e) {
+      return q;
+    } catch (e) {
       console.log(e.message);
       return q;
     }
   }));
 }
 
-async function addSomeColor() {
+async function addSomeColor () {
   const quotesWithColors = await fetchInfo(quotes);
-  for (q of quotesWithColors) {
-    db.quotes.updateOne({_id: q._id}, {$set: {colors: q.colors}});
+  for (const q of quotesWithColors) {
+    db.quotes.updateOne({ _id: q._id }, { $set: { colors: q.colors } });
   }
 }
 
